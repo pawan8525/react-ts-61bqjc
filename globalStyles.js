9e70d551764a70f929e1b8be12d3f85cd40a2bc8
pawan8525 <ponisangwan@gmail.com>
2022-08-17 13:11:17 +0530

    1 file modified, 1 added

---
+++ globalStyles.js
@@ -0,0 +1,11 @@
+import styled, { createGlobalStyle } from 'styled-component';
+
+const GlobalStyle = createGlobalStyle`
+*{
+  box-sizing:border-box;
+  margin:0;
+  padding:0;
+  font-family:'Montserrat',sans-serif;
+}
+
+`;
